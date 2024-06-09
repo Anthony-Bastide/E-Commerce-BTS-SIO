724763254fe9675e42fb8a4935e3bb8bea8d23b2
Anthony-Bastide <BastideAnthony.pro@gmail.com>
2024-06-09 15:01:18 +0200

    login user

--- E-Commerce/app/controllers/connectionController.php
+++ E-Commerce/app/controllers/connectionController.php
@@ -7,7 +7,26 @@ class connectionController extends Controller{
 
     public function login()
     {
-        return $this->display("auth.index");
+        $user = new User($this->getDB());
+        $user = $user->findByColumn($_POST['email'], 'email');
+
+        if(password_verify($_POST['password'],$user->password) ){      
+           if($user->role == true)
+           {
+                $_SESSION['auth'] = $user->role;
+                $_SESSION['user_id'] = $user->id;
+                if($user->role == 1){
+                    $_SESSION['is_admin'] = true;
+
+                return $this->view("homepage.index");
+                }
+            return $this->view("homepage.index");
+
+           }
+        } else {
+            header('location: /e-commerce-BTS-SIO/E-Commerce/homepage');
+            echo "<script>alert('Le mot de passe ou l'identifiant est incorrect');</script>";
+        }
     }
 
     public function create()
